e74861f5c542d60eb6f56d807c9d9731dccb7d8c
ShawnM999 <azmelonis@gmail.com>
2024-02-21 22:03:38 -0700

    admin page works

--- Python/servertest.py
+++ Python/servertest.py
@@ -23,11 +23,30 @@ def login():
     if not username or not password:
         return jsonify({'error': 'Missing username or password'}), 400
 
-    if check_credentials(username, password):
+    # Check credentials
+    user = check_credentials(username, password)
+    if user:
         session['username'] = username  # Set session variable
-        return redirect(url_for('static', filename='home.html'))
+        # Check if user is admin
+        if is_admin(username):
+            return jsonify({'isAdmin': True}), 200
+        else:
+            return jsonify({'isAdmin': False}), 200
     else:
         return jsonify({'error': 'Invalid credentials'}), 401
+
+def is_admin(username):
+    conn = connect_to_database()
+    cursor = conn.cursor()
+    # Execute the SQL query to check if the user is an admin
+    sql = "SELECT admin FROM users WHERE username = %s"
+    cursor.execute(sql, (username,))
+    result = cursor.fetchone()
+
+    if result and result[0] == 1:
+        return True
+    else:
+        return False
 
 def connect_to_database():
     return pymysql.connect(host=DB_HOST, user=DB_USER, password=DB_PASSWORD, database=DB_NAME)
