a48ad992f97f8e66b8649697e0fc5e599cdcde09
ShawnM999 <azmelonis@gmail.com>
2024-02-21 19:58:47 -0700

    stuff

--- JS/code.js
+++ JS/code.js
@@ -6,8 +6,31 @@ document.getElementById("login").addEventListener("submit", function(event) {
     // Check if username and password match the expected values
     if (username === "shawn" && password === "1234") {
       // Redirect to the next page (replace 'next_page.html' with your desired URL)
-      window.location.href = "./User/userhome.html";
-    } else {
-      alert("Incorrect username or password. Please try again.");
-    }
-  });
+      window.location.href = "./User/userhome.html";}
+
+    else if (username === "root" && password === "root") {
+        // Redirect to the next page (replace 'next_page.html' with your desired URL)
+        window.location.href = "./Admin/adminhome.html";} 
+
+    else {
+      alert("Incorrect username or password. Please try again.");}});
+
+
+document.getElementById("findClass").addEventListener("click", function() {
+    var content = document.getElementById("classes");
+
+    if (content.style.display === "none") {
+        content.style.display = "block";}
+
+    else {
+        content.style.display = "none";}});
+
+
+
+
+
+
+
+
+
+
